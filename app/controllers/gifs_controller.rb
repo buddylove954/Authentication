a9643af626d5c18a72f78c78fccf825b4c966fbf
will love <wlove29@gmail.com>
2015-04-22 14:32:00 -0400

    added an authorize action

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,2 +1,9 @@
 class GifsController < ApplicationController
+	before_action :authorize
+	
+	def cat
+	end
+
+	def admin
+	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by_email(params[:email])
-		
+
 		if user && user.authenticate(params[:password])
 				session[:user_id] = user.id
 				redirect_to '/'
@@ -14,6 +14,8 @@ class SessionsController < ApplicationController
 		end
 	end
 
-	def delete
+	def destroy
+		session[:user_id] = nil
+		redirect_to '/login'
 	end
 end
